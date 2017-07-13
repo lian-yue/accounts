3fae9a0b7d054cdc749e711ea55d71bd5a725233
git <admin@lianyue.org>
2017-07-13 22:51:16 +0900

    config

---
+++ config/reserve.js
@@ -0,0 +1,78 @@
+var reserve = [
+  'me',
+  'all',
+  'userauth',
+  'black',
+  'save',
+  'select',
+  'create',
+  'update',
+  'delete',
+  'remove',
+  'read',
+  'editor',
+  'index',
+  'home',
+  'default',
+  'info',
+  'noreply',
+  'reply',
+  'web',
+  'get',
+  'add',
+  'set',
+  'del',
+  'put',
+  'patch',
+  'www',
+  'user',
+  'account',
+  'vip',
+  'log',
+  'role',
+  'post',
+  'image',
+  'file',
+  'storage',
+  'store',
+  'slug',
+  'tag',
+  'date',
+  'page',
+  'sort',
+  'skip',
+  'order',
+  'limit',
+  'filter',
+  'type',
+  'name',
+  'comment',
+  'state',
+  'status',
+  'authorize',
+  'auth',
+  'oauth',
+  'oauth2',
+  'server',
+  'token',
+  'client',
+  'mail',
+  'email',
+  'phone',
+  'mobile',
+  'base',
+  'class',
+  'app',
+]
+
+
+module.exports = function(value) {
+  if (typeof value != 'string' || value < 3) {
+    return false
+  }
+  value = value.trim().toLocaleLowerCase()
+  if (value.substr(-1, 1) == 's') {
+    value = value.substr(0, value.length -1)
+  }
+  return reserve.indexOf(value) == -1
+}
---
+++ config/site.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '恋月',
+  url: '//www.lianyue.org',
+  keywords: ['恋月', '恋月博客', '恋月 Blog', 'LianYue', 'LianYue Blog'],
+  description: '恋月 的小站',
+  author: 'LianYue',
+  email: 'admin@lianyue.org',
+  assets:  process.env.NODE_ENV == 'development' ? 'http://localhost:3000/assets/' : '/assets/',
+}
